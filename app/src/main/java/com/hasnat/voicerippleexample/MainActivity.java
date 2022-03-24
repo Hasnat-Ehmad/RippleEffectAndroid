f62e530f41b2cf6188487f106ca691e7f1f98b14
hasnat_Ehmad <hasnat.ahmad@hotmail.com>
2022-03-24 14:34:16 +0500

    remove library

--- app/src/main/java/com/hasnat/voicerippleexample/MainActivity.java
+++ app/src/main/java/com/hasnat/voicerippleexample/MainActivity.java
@@ -1,7 +1,6 @@
 package com.hasnat.voicerippleexample;
 
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
--- ripplelibrary/src/main/java/info/kimjihyok/ripplelibrary/Rate.java
+++
@@ -1,9 +0,0 @@
-package info.kimjihyok.ripplelibrary;
-
-/**
- * Created by jihyokkim on 2017. 8. 25..
- */
-
-public enum Rate {
-  LOW, MEDIUM, HIGH
-}
